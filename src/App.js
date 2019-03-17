9cb18641efb3dfd76c6d438c1badfcd032fe182f
KarloFab <fabijanickarlo1@gmail.com>
2019-03-17 11:12:03 +0100

    Added bindings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Karlo', age: 21},
+      { name: 'Stefani', age: 26},
+    ]
+  }
+
+  switchNameHandler = () => {
+    //DONT DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({  
+      persons: [
+      { name: 'Maximilian', age: 28},
+      { name: 'Karlo', age: 21},
+      { name: 'Stefani', age: 26},
+    ] })
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({  
+      persons: [
+      { name: 'Maximilian', age: 28},
+      { name: event.target.value, age: 21},
+      { name: 'Stefani', age: 26},
+      ]}
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React app</h1>
+        <button onClick={() => this.switchNameHandler()}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name}  
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Max')}
+          changed ={this.nameChangedHandler}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name}  
+          age={this.state.persons[2].age}></Person>
       </div>
     );
   }
